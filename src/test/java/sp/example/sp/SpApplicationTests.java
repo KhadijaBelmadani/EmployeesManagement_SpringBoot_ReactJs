03dd6a1881b844706d1d9f2c97002347e0b6cb48
Khadija Belmadani <kbelmadani906@gmail.com>
2024-01-13 14:14:39 +0100

    Initial commit

---
+++ src/test/java/sp/example/sp/SpApplicationTests.java
@@ -0,0 +1,13 @@
+package sp.example.sp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
